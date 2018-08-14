8480aa8727e4f6b043d2f858da3b54786685507d
tishayaem <kostiantynkalinin@gmail.com>
2018-08-14 17:06:23 +0100

    Render unique room information in Details component #35

--- src/FloorPlan/Details/Details.js
+++ src/FloorPlan/Details/Details.js
@@ -1,37 +1,41 @@
 import React from "react";
-<<<<<<< HEAD
 import floorPlanData from "./floorPlanData";
 
 export default class Details extends React.Component {
-  constructor() { 
+  constructor() {
     super();
     this.state = {
       roomInfo: undefined
     };
   }
 
-setRoomInfo(roomInfo) {
-    this.setState({ roomInfo: roomInfo })
+  setRoomInfo(roomInfo) {
+    this.setState({ roomInfo: roomInfo });
   }
 
-componentDidMount() {
-  this.setRoomInfo (floorPlanData().find(roomInfo => {
-    return roomInfo.studioNo == this.props.room.id;
-  })
-);
-
-}
+  componentDidMount() {
+    this.setRoomInfo(
+      floorPlanData().find(roomInfo => {
+        return roomInfo.studioNo == this.props.room.id;
+      })
+    );
+  }
 
   render() {
-    if (this.state.roomInfo) {return <div>{this.state.roomInfo.availability}</div>} else {
-      return (<div>Nothing</div>)
+    if (this.state.roomInfo) {
+      return (
+        <div>
+          <div>Room {this.state.roomInfo.studioNo}</div>
+          <div>Availability: {this.state.roomInfo.availability}</div>
+          <div>Window: {this.state.roomInfo.window}</div>
+          <div>Size: {this.state.roomInfo.sizeSqFt} sq ft</div>
+          <div>Anual price: {this.state.roomInfo.pricePsfAnual}£</div>
+          <div>Price excluding VAT: {this.state.roomInfo.priceExVat}£</div>
+          <div>Price including VAT: {this.state.roomInfo.priceIncVat}£</div>
+        </div>
+      );
+    } else {
+      return <div>Nothing</div>;
     }
-}
-=======
-
-export default class Details extends React.Component {
-  render() {
-    return <div>{this.props.room.id}</div>;
   }
->>>>>>> upstream/master
 }
